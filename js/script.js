071c190350f1ea9d94bf598561133fc8beb36a3a
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-11-05 20:31:35 +0300

    feat: task complette

--- js/script.js
+++ js/script.js
@@ -1,45 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     let currentElem = null;
     document.querySelectorAll('.list').forEach(function (item) {
+            item.onmouseover = function (event) {
+                if (currentElem) return;
+                let target = event.target.parentNode;
+                if (!target) return;
+                if (!item.contains(target)) return;
 
-        item.onmouseover = function (event) {
-            if (currentElem) return;
-            let target = event.target.parentNode;
-            if (!target) return;
-            if (!item.contains(target)) return;
+                currentElem = target.lastElementChild;
+                currentElem.classList.remove('hide_caption');
+                currentElem.classList.add('show_caption');
+            };
+            item.onmouseout = function (event) {
+                if (!currentElem) return;
+                let relatedTarget = event.relatedTarget;
 
-            currentElem = target.lastElementChild;
-            currentElem.classList.remove('hide_caption');
-            currentElem.classList.add('show_caption');
-        };
+                while (relatedTarget) {
+                    if (relatedTarget == currentElem) return;
+                    relatedTarget = relatedTarget.parentNode;
+                }
 
-        item.onmouseout = function (event) {
-            if (!currentElem) return;
-            let relatedTarget = event.relatedTarget;
-
-            while (relatedTarget) {
-                if (relatedTarget == currentElem) return;
-                relatedTarget = relatedTarget.parentNode;
-            }
-
-            currentElem.classList.remove('show_caption');
-            currentElem.classList.add('hide_caption');
-            currentElem = null;
-        };
-    }),
-    navList = document.querySelector('.header-navigation__list'),
-    exit = document.querySelector('.header-navigation__close'),
-    burger = document.querySelector('.header-navigation__hamburger');
+                currentElem.classList.remove('show_caption');
+                currentElem.classList.add('hide_caption');
+                currentElem = null;
+            };
+        }),
+        navList = document.querySelector('.header-navigation__list'),
+        exit = document.querySelector('.header-navigation__close'),
+        burger = document.querySelector('.header-navigation__hamburger');
     burger.onclick = function () {
         exit.classList.add('menu_visible');
         navList.classList.add('menu_visible');
         burger.style.display = 'none';
-        
+
     };
     exit.onclick = function () {
         exit.classList.remove('menu_visible');
         navList.classList.remove('menu_visible');
         burger.style.display = 'block';
-    } 
+    }
 });
